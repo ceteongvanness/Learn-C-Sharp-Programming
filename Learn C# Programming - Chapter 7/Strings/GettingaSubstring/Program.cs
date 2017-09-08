907b567e0474f1b50e9b2fd9406e35fc9a9f492d
Cheah Eng Teong <ceteongvanness@hotmail.com>
2017-09-08 17:00:59 +0800

    Chapter 7 - Arrays & Strings

---
+++ Learn C# Programming - Chapter 7/Strings/GettingaSubstring/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingaSubstring
+{
+    class StringProg
+    {
+        static void Main(string[] args)
+        {
+            string str = "Last night I dreamt of San Pedro";
+            Console.WriteLine(str);
+            string substr = str.Substring(23);
+            Console.WriteLine(substr);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Learn C# Programming - Chapter 7/Strings/Strings/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //from string literal and string concatenation
+            string fname, lname;
+            fname = "EngTeong";
+            lname = "Cheah";
+
+            string fullname = fname + lname;
+            Console.WriteLine("Full Name: {0}", fullname);
+
+            //by using string constructor
+            char[] letters = { 'H', 'e', 'l', 'l', 'o' };
+            string greetings = new string(letters);
+            Console.WriteLine("Greetings: {0}", greetings);
+
+            //methods returning string
+            string[] sarray = { "Hello", "From", "Channel", "9" };
+            string message = String.Join(" ", sarray);
+            Console.WriteLine("Message: {0}", message);
+
+            //formatting method to convert a value
+            DateTime waiting = new DateTime(2017, 09, 08, 17, 58, 1);
+            string chat = String.Format("Message sent at {0:t} on {0:D}", waiting);
+            Console.WriteLine("Message: {0}", chat);
+
+            Console.ReadLine();
+        }
+    }
+}
